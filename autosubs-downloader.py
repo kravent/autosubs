8c148e451cc6da6173b100a10328b34293927c93
Adrian Garcia <adriangs91@gmail.com>
2010-11-25 18:50:24 +0100

    añadida función descarga bittorrent
    (contiene fallos)

--- autosubs-downloader.py
+++ autosubs-downloader.py
@@ -4,26 +4,27 @@ import sys
 import urllib
 import re
 import time
+import libtorrent
 
 def getNyaTorrentsFile(serie,fansub,capitulo,size='720',otros_patrones=None):
-  serie=serie.lower()
-  page='http://www.nyaatorrents.org/?page=search&cat=0_0&filter=0&term='+serie.replace(' ','+')
+  serie = serie.lower()
+  page = 'http://www.nyaatorrents.org/?page=search&cat=0_0&filter=0&term='+serie.replace(' ','+')
   try:
-    file=urllib.urlopen(page)
-    pagina=str(file.read())
+    file = urllib.urlopen(page)
+    pagina = str(file.read())
     file.close()
     for line in re.findall('<td class="tlistname">.*?</td>',pagina):
-      name=re.sub('<.*?>','',line).strip()
-      dname=name.lower()
+      name = re.sub('<.*?>','',line).strip()
+      dname = name.lower()
       url='http://www.nyaatorrents.org/?page=download&'+re.findall('tid=\d+',line)[0]
       if (re.search('\['+fansub+'\]',dname) and re.search(serie,dname) and
           re.search('\D'+capitulo+'\D',dname) and
           (re.search(size+'p',dname) or re.search('x'+size,dname))):
         if otros_patrones:
-          valido=True
+          valido = True
           for patron in otros_patrones:
             if not re.search(patron,dname):
-              valido=False
+              valido = False
           if valido:
             return [name,url]
         else:
@@ -47,10 +48,33 @@ def waitfile(serie,fansub,capitulo,size='720',otros_patrones=None):
 
 
 
-def downloadtorrent()
+def downloadtorrent(torrentfile,destino='./',puertos=[51413,51413]):
+  ses = libtorrent.session()
+  ses.listen_on(puertos[0], puertos[1])
+  
+  if re.search('^http://',torrentfile) or re.search('^https://',torrentfile):
+    e = libtorrent.bdecode(urllib.urlopen(torrentfile).read())
+  else:
+    e = libtorrent.bdecode(open(torrentfile, 'rb').read())
+  info = libtorrent.torrent_info(e)
+  h = ses.add_torrent(info, destino, storage_mode=storage_mode_sparse)
+
+  while (not h.is_seed()):
+          s = h.status()
+
+          state_str = ['queued', 'checking', 'downloading metadata', \
+                  'downloading', 'finished', 'seeding', 'allocating']
+          print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+                  (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+                  s.num_peers, state_str[s.state])
+
+          time.sleep(1)
 
 
 
 if __name__ == '__main__':
-  res=waitfile(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
-  print(res)
+  if len(sys.argv) < 4:
+    print 'USO: autosubs-downloader.py serie fansub capitulo'
+    exit(-1)
+  res = waitfile(sys.argv[1],sys.argv[2],sys.argv[3])
+  downloadtorrent(res[1])
