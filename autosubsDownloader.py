030ac7ba66450d5c0ada68e803613f83493a88d9
Adrian Garcia <adriangs91@gmail.com>
2010-11-26 23:11:25 +0100

    añadidos mkv2raw() y mkv2ass()

--- autosubsDownloader.py
+++ autosubsDownloader.py
@@ -6,8 +6,9 @@ import urllib
 import re
 import time
 import libtorrent
+import subprocess
 
-def getNyaTorrentsFile(serie,fansub,capitulo,size='720',otros_patrones=None):
+def getNyaTorrentsFile(serie, fansub, capitulo, size='720', otros_patrones=None):
   serie = serie.lower()
   page = 'http://www.nyaatorrents.org/?page=search&cat=0_0&filter=0&term='+serie.replace(' ','+')
   try:
@@ -35,23 +36,24 @@ def getNyaTorrentsFile(serie,fansub,capitulo,size='720',otros_patrones=None):
 
 
 
-def waitfile(serie,fansub,capitulo,size='720',otros_patrones=None):
+def waitfile(serie, fansub, capitulo, size='720', otros_patrones=None):
   while True:
     print 'Buscando capítulo...',
     sys.stdout.flush()
-    file=getNyaTorrentsFile(serie,fansub,capitulo,size,otros_patrones)
+    file=getNyaTorrentsFile(serie, fansub, capitulo, size, otros_patrones)
     if file:
       print 'ENCONTRADO "'+file[0]+'"'
       return file
     print 'LINK NO DISPONIBLE'
-    print time.strftime("%H:%M:%S", time.gmtime()),'LINK NO DISPONIBLE -> (esperando 10 min)'
+    print time.strftime("%H:%M:%S", time.gmtime()),
+    print 'LINK NO DISPONIBLE -> (esperando 10 min)'
     time.sleep(600)
 
 
 class TorrentProgress:
   def __init__(self):
     pass
-  def setProgress(self,progress,download_rate,upload_rate,num_peers):
+  def setProgress(self, progress, download_rate, upload_rate, num_peers):
     print "\r%3.2f%% complete (down: %4.1f kb/s up: %4.1f kB/s peers: %3d)" % \
           (progress * 100, download_rate / 1000, \
           upload_rate / 1000, num_peers) ,#Coma final -> NO "\n"
@@ -59,7 +61,7 @@ class TorrentProgress:
 
 
 
-def downloadtorrent(torrentfile,destino='./',puertos=[51413,51413]):
+def downloadtorrent(torrentfile, destino='./' ,puertos=[51413,51413]):
   print 'DESCARGANDO...'
   ses = libtorrent.session()
   ses.listen_on(puertos[0], puertos[1])
@@ -85,9 +87,54 @@ def downloadtorrent(torrentfile,destino='./',puertos=[51413,51413]):
   return filesaved
 
 
+def mkvGetTrack(mkvfile,tipo):
+  track = None
+  cmd = ['mkvmerge','-i',mkvfile]
+  sal = subprocess.Popen(cmd,stdout=subprocess.PIPE,bufsize=0)
+  for line in sal.stdout:
+    m = re.match('.*(\d+).*\('+tipo+'\)',line)
+    if m:
+      track = m.group(1)
+      break
+  return track
+
+
+def mkv2raw(mkvfile, mkvrawfile, fps=None):
+  mpeg4Track=mkvGetTrack(mkvfile,'V_MPEG4/ISO/AVC')
+  cmd = "mkvmerge -v -o '%s' --no-subtitles --no-attachments" % mkvrawfile
+  if fps and mpeg4Track:
+    cmd = cmd + ' --default-duration %s:%sfps' % (mpeg4Track, fps)
+  cmd = cmd + " '%s'" % mkvfile
+  process = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+  print 'Generando RAW...',
+  sys.stdout.flush()
+  exitcode = process.wait()
+  if exitcode == 0:
+    print 'OK'
+    return True
+  else:
+    print 'ERROR'
+    return False
+
+
+def mkv2ass(mkvfile,assfile):
+  assTrack=mkvGetTrack(mkvfile,'S_TEXT/ASS')
+  cmd = "mkvextract tracks '%s' %s:'%s'" % (mkvfile, assTrack, assfile)
+  process = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+  print 'Extrayendo subtitulos...',
+  sys.stdout.flush()
+  exitcode = process.wait()
+  if exitcode == 0:
+    print 'OK'
+    return True
+  else:
+    print 'ERROR'
+    return False
+
+
+
 
 if __name__ == '__main__':
-  """
   if len(sys.argv) < 4:
     print 'USO: autosubs-downloader.py serie fansub capitulo [resolucion]', \
         '[patron_regular1 patron_regular2 ...]'
@@ -99,6 +146,7 @@ if __name__ == '__main__':
   else:
     res = waitfile(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5:])
 
-  downloadtorrent(res[1])
-  """
-  downloadtorrent('http://www.nyaatorrents.org/?page=download&tid=176003')
+  nombre=downloadtorrent(res[1])
+  mkv2raw(nombre,'[RAW]'+nombre)
+  mkv2ass(nombre,nombre+'.ass')
+
